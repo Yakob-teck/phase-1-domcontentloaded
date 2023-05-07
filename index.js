65aabe27113afca2f38e02a926d4c814fa22b94f
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-05-07 11:02:25 -0600

    documentedntloaded

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(event) {
+   
+  });
+  document.addEventListener("DOMContentLoaded", function(event) {
+    const textElement = document.getElementById("text");
+    textElement.textContent = "This is really cool!";
+  });
+  
